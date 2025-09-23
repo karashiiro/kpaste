ac29ed695dfee69921166de839c418d242d47b35
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-09-23 14:43:25 -0700

    Consolidate config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
@@ -7,5 +7,9 @@ export default defineConfig({
   define: {
     global: "globalThis",
     "process.env": {},
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
